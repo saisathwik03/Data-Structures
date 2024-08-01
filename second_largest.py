29d0f2c7e140df91e7c1d095094d44e1c1d955b0
saisathwik03 <saisathwik173@gmail.com>
2024-08-01 19:35:52 +0530

    Second Largest Element

---
+++ second_largest.py
@@ -0,0 +1,17 @@
+#Question: Given an array arr, return the second largest distinct element from an array. If the second largest element doesn't exist then return -1.
+
+#Code:
+
+def print2largest(arr):
+        # Code Here
+        
+        max1=-1
+        max2=-1
+        
+        for i in arr:
+            if i>max1:
+                max2=max1
+                max1=i
+            elif i>max2 and max1!=i:
+                max2=i
+        return max2
