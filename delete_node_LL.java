d1461d19e1d58d63593f9121af816cfa1ee59029
saisathwik03 <saisathwik173@gmail.com>
2024-09-05 14:40:57 +0530

    Delete in LL

---
+++ delete_node_LL.java
@@ -0,0 +1,12 @@
+// Question: There is a singly-linked list head and we want to delete a node node in it.
+// You are given the node to be deleted node. You will not be given access to the first node of head.
+
+// CODE:
+
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
