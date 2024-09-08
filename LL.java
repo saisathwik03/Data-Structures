280aea308f4bddf70954a22008f68e118f3cbf55
saisathwik03 <saisathwik173@gmail.com>
2024-09-09 02:32:38 +0530

    stack_using_arr

--- LL.java
+++ LL.java
@@ -1,22 +1,22 @@
-# Question: Given an array of integer arr. Your task is to construct the linked list from arr & return the head of the linked list.
+// # Question: Given an array of integer arr. Your task is to construct the linked list from arr & return the head of the linked list.
 
-# CODE:
+// # CODE:
 
-class LL {
-    static Node constructLL(int arr[]) {
-    // code here
-    if(arr.length == 0){
-        return null;
-    }
+// class LL {
+//     static Node constructLL(int arr[]) {
+//     // code here
+//     if(arr.length == 0){
+//         return null;
+//     }
     
-    Node head = new Node(arr[0]);
-    Node current = head;
+//     Node head = new Node(arr[0]);
+//     Node current = head;
     
-    for(int i=1; i<arr.length; i++){
-        current.next = new Node(arr[i]);
-        current = current.next;
-    }
+//     for(int i=1; i<arr.length; i++){
+//         current.next = new Node(arr[i]);
+//         current = current.next;
+//     }
     
-    return head;
-    }
-}
+//     return head;
+//     }
+// }
