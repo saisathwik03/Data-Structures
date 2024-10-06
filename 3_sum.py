b3c105a03e1f005f237b8f2b90709b9c8041ddfe
saisathwik03 <saisathwik173@gmail.com>
2024-10-06 22:45:24 +0530

    For showing

--- 3_sum.py
+++ 3_sum.py
@@ -36,3 +36,21 @@ def findTriplets(self, arr, n):
                 right -= 1
     
     return 0
+
+# def findTriplets(self, arr, n):
+#     arr.sort()
+    
+#     for i in range(n - 2):
+#         left, right = i + 1, n - 1
+        
+#         while left < right:
+#             total = arr[i] + arr[left] + arr[right]
+            
+#             if total == 0:
+#                 return 1
+#             elif total < 0:
+#                 left += 1
+#             else:
+#                 right -= 1
+    
+#     return 0
